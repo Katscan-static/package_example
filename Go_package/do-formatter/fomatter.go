9645776ae11140418476715a7203bb33c5a864b6
Katscan-static <57810611+Katscan-static@users.noreply.github.com>
2025-04-18 12:08:43 +0200

    Add files via upload

---
+++ Go_package/do-formatter/fomatter.go
@@ -0,0 +1,8 @@
+package format
+
+import "fmt"
+
+func Number(num int) string {
+
+	return fmt.Sprintf("the number is %d", num)
+}
---
+++ Go_package/test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"math"
+)
+
+func main() {
+	num := math.Double(2)
+	output := format.Number(num)
+	fmt.Println(output)
+}
